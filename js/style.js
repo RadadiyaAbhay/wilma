5d9f926aef03936ab83c240ef596029f433bfe11
RadadiyaAbhay <abhay592004@gmail.com>
2023-03-14 10:44:34 +0530

    wilma

---
+++ js/style.js
@@ -0,0 +1,122 @@
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 100) {
+        $('header').addClass('head-after');
+
+    }
+    else {
+       $('header').removeClass('head-after');
+    }
+
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 1450) {
+        $('.img-32-1').addClass('img-32-after');
+
+    }
+    else {
+       $('.img-32-1').removeClass('img-32-after');
+    }
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 1700) {
+        $('.img-32-').addClass('img-32-after2');
+
+    }
+    else {
+       $('.img-32-1').removeClass('img-32-after2');
+    }
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 450) {
+        $('.img-32-5').addClass('img-32-after');
+
+    }
+    else {
+       $('.img-32-5').removeClass('img-32-after');
+    }
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 850) {
+        $('.img-32-5').addClass('img-32-after2');
+
+    }
+    else {
+       $('.img-32-5').removeClass('img-32-after2');
+    }
+    });
+	
+	
+	/*our menu*/
+	
+	
+	
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 600) {
+        $('.our-1').addClass('our-after');
+
+    }
+    else {
+       $('.our-1').removeClass('our-after');
+    }
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 900) {
+        $('.our-1').addClass('our-after2');
+
+    }
+    else {
+       $('.our-1').removeClass('our-after2');
+    }
+    });
+	
+	/*our menu-2*/
+	
+	
+	
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 1850) {
+        $('.our-2').addClass('our-after');
+
+    }
+    else {
+       $('.our-2').removeClass('our-after');
+    }
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 2150) {
+        $('.our-2').addClass('our-after2');
+
+    }
+    else {
+       $('.our-2').removeClass('our-after2');
+    }
+    });
+	
+	/*our menu-3*/
+	
+	
+	
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 3100) {
+        $('.our-3').addClass('our-after');
+
+    }
+    else {
+       $('.our-3').removeClass('our-after');
+    }
+    });
+	$(window).scroll(function(){
+    if ($(window).scrollTop() >= 3400) {
+        $('.our-3').addClass('our-after2');
+
+    }
+    else {
+       $('.our-3').removeClass('our-after2');
+    }
+    });
+	$('.side-tog').click(function(){
+		$('.side-togg').toggleClass('side-tog-2');
+	})
+	
+	
+	
